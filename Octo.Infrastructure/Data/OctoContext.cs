2cd0abb8c2aa2bb4c5dfb6849004b3157636a908
edsdck <edsdck@gmail.com>
2020-04-05 12:35:59 +0300

    add initial database support and connection

---
+++ Octo.Infrastructure/Data/OctoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Octo.Infrastructure.Data
+{
+    public class OctoContext : DbContext
+    {
+        public OctoContext(DbContextOptions<OctoContext> options) : base(options)
+        {
+        }
+    }
+}
--- Octo.Web/Startup.cs
+++ Octo.Web/Startup.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Octo.Infrastructure.Data;
 
 namespace Octo.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OctoContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,10 +34,7 @@ namespace Octo.Web
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
